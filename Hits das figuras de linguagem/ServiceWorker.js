7e5b10714afdffdbb6767362b614246297a5c374
RosembergSaraiva <begsaraiva@gmail.com>
2025-10-29 10:39:26 -0300

    First submission

---
+++ Hits das figuras de linguagem/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "ACTCON-ODA7001 - Hits das figuras de linguagem-0.1.0";
+const contentToCache = [
+    "Build/Hits das figuras de linguagem.loader.js",
+    "Build/Hits das figuras de linguagem.framework.js",
+    "Build/Hits das figuras de linguagem.data",
+    "Build/Hits das figuras de linguagem.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
